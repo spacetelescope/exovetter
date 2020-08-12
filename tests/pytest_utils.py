5827bcfa393e9d2da9f6f2f7f46626ff8fa2aee8
Joe Curtin <42@jbcurtin.io>
2020-08-12 13:36:04 -0400

    Added flake8 changes

--- tests/pytest_utils.py
+++ tests/pytest_utils.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture
 def mat_url() -> str:
-    return 'https://sourceforge.net/p/lpptransitlikemetric/code/HEAD/tree/data/maps/mapQ1Q17DR24-DVMed6084.mat?format=raw'
+    return 'https://sourceforge.net/p/lpptransitlikemetric/code/HEAD/tree/data/maps/mapQ1Q17DR24-DVMed6084.mat?format=raw'  # noqa: E501
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
-from pytest_utils import mat_url
+from pytest_utils import mat_url  # noqa: F401
 
-def test__load_mat(mat_url):
+
+def test__load_mat(mat_url):  # noqa: F811
     import numpy
 
     from exovetter.utils import load_mat
