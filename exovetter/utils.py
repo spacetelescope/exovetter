8bb5a4df33fe72d43bee48dd5eea499d6167d11d
Joe Curtin <42@jbcurtin.io>
2020-08-12 13:37:28 -0400

    Added flake8 changes for exovetter package

--- exovetter/utils.py
+++ exovetter/utils.py
@@ -5,9 +5,9 @@ from astropy.utils import data
 
 ENCODING = 'utf-8'
 
+
 def load_mat(url: str) -> typing.Dict[str, typing.Any]:
     if url.startswith('http'):
         return loadmat(data.download_file(url, cache=True))
 
     raise NotImplementedError(f'URL Format not implemented[{url}]')
-
