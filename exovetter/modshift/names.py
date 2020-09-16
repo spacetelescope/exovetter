6e3985b7eba4fc830cd2aa236f9f3a4ce89ccb64
Fergal <fergal.mullally@gmail.com>
2020-09-15 22:30:29 -0400

    Initial commit

---
+++ exovetter/modshift/names.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 21:43:47 2020
+
+@author: fergal
+"""
+
+
+PERIOD= 'period_days'
+EPOCH = 'epoch_days'
+DURATION_HRS = 'duration_hrs'
+DEPTH = 'depth_ppm'
+
