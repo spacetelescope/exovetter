8ccc698e39d34869d68e8325cd2680ec0904b478
Susan Mullally <smullally@stsci.edu>
2020-10-02 10:16:01 -0400

    tests for transit_coverage

--- exovetter/transit_coverage.py
+++ exovetter/transit_coverage.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 19 22:06:45 2020
 
-@author: smullally
-"""
 import numpy as np
 
 
@@ -29,41 +25,5 @@ def calc_coverage(time, p_day, epoch, dur_hour, ndur=2, nbins=10):
 def compute_phases(time, period, epoch, offset=0.5):
 
     phases = np.fmod(time - epoch + (offset * period), period)
-    return phases
-
-
-def test_coverage():
-    time = np.arange(0, 100, step=.01)
-    p_day = 3
-    epoch = 3
-    dur_hour = 5
-
-    coverage = calc_coverage(time, p_day, epoch, dur_hour, ndur=2, nbins=10)
-
-    assert coverage == 1.0
-
-    return coverage
-
-
-def test_coverage2():
-    time = np.arange(0, 100, step=1)
-    p_day = 3
-    epoch = 3
-    dur_hour = 12
-
-    coverage = calc_coverage(time, p_day, epoch, dur_hour, ndur=2, nbins=10)
-
-    assert coverage == 0.1
-
-    return coverage
-
-
-def test_coverage3():
-    time = np.arange(0, 100, step=1)
-    p_day = 3
-    epoch = 3
-    dur_hour = 12
-
-    coverage = calc_coverage(time, p_day, epoch, dur_hour, ndur=2, nbins=10)
-
-    return coverage
+    
+    return phases/period
