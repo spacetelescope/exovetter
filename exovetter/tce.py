50586ad89466782755d883d2c038d9e34858c5b4
Susan Mullally <smullally@stsci.edu>
2020-10-07 15:33:22 -0400

    auto pep8

--- exovetter/tce.py
+++ exovetter/tce.py
@@ -31,6 +31,7 @@ class TCE:
         Name of the TCE or planet for plotting.
 
     """
+
     def __init__(self, period=1 * u.day, tzero=0, duration=1 * u.hour,
                  depth=1, target_name='target name', event_name="event b"):
         # TODO: Use Quantity throughout to avoid unit conversion
@@ -45,7 +46,7 @@ class TCE:
     def to_dict(self):
         """Return TCE attributes as a dictionary."""
 
-        return {'period':  self.period,
+        return {'period': self.period,
                 'tzero': self.tzero,
                 'duration': self.duration,
                 'depth': self.depth,
@@ -58,27 +59,27 @@ class TCE:
             raise ValueError(f'The period ({self.period}) is shorter than '
                              f'the duration ({self.duration}).')
 
-    
     def get_boxmodel(self, times):
         """Return box model, which is also stored in ``self.model``."""
-        
-        offset = 0.5  #This offset is not returned to user.
+
+        offset = 0.5  # This offset is not returned to user.
         model = np.ones(len(times))
-        
+
         self.model_times = times
-        self.model_phases = np.fmod(times - self.tzero + (offset * self.period), self.period)
-        
-        phases = self.model_phases 
-        
+        self.model_phases = np.fmod(
+            times - self.tzero + (offset * self.period), self.period)
+
+        phases = self.model_phases
+
         want = ((phases > offset * self.period - 0.5 * self.duration / 24) &
                 (phases <= offset * self.period + 0.5 * self.duration / 24))
 
         model[want] = -1 * self.depth
-        
-        #plt.figure()
-        #plt.plot(phases, model,'.')
-        #plt.title('tce box model')
-        
+
+        # plt.figure()
+        # plt.plot(phases, model,'.')
+        # plt.title('tce box model')
+
         self.model = model
 
         return model
