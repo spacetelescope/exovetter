3583dcf385c80824a9e8b2362d751aacfe8f3bbd
Susan Mullally <smullally@stsci.edu>
2020-10-07 15:59:31 -0400

    fix peppy stuff

--- exovetter/transit_coverage.py
+++ exovetter/transit_coverage.py
@@ -7,9 +7,9 @@ import numpy as np
 def calc_coverage(time, p_day, epoch, dur_hour, ndur=2, nbins=10):
     """Calculate the fraction of  the in-transit points that contain data.
     """
-    phases = compute_phases(time, p_day,  epoch, offset=0.5)
+    phases = compute_phases(time, p_day, epoch, offset=0.5)
 
-    phase_offset = ndur/2 * dur_hour/24.0/p_day
+    phase_offset = ndur / 2 * dur_hour / 24.0 / p_day
     high = 0.5 + phase_offset
     low = 0.5 - phase_offset
 
@@ -19,11 +19,11 @@ def calc_coverage(time, p_day, epoch, dur_hour, ndur=2, nbins=10):
 
     n_bins_with_data = np.sum(hist > 0)
 
-    return n_bins_with_data/nbins
+    return n_bins_with_data / nbins
 
 
 def compute_phases(time, period, epoch, offset=0.5):
 
     phases = np.fmod(time - epoch + (offset * period), period)
-    
-    return phases/period
+
+    return phases / period
