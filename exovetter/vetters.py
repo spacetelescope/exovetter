f114abe7a8bca87374580dafadcdac48584562b4
Susan Mullally <smullally@stsci.edu>
2021-03-05 14:56:14 -0500

    Update exovetter/vetters.py
    Co-authored-by: P. L. Lim <2090236+pllim@users.noreply.github.com>

--- exovetter/vetters.py
+++ exovetter/vetters.py
@@ -100,7 +100,7 @@ class ModShift(BaseVetter):
             to vet, like period, epoch, duration, depth
 
         lc : lightkurve object
-        lightkurve object with the time and flux to use for vetting.
+            lightkurve object with the time and flux to use for vetting.
 
         """
         self.time, self.flux, time_offset_str = \
